fee640de70ffc8e3ce24edd98138e3e24fa39158
Your Name <you@example.com>
2022-06-21 16:22:25 +0300

    Uploaded

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-const express = require('express')
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
 
-const app = express()
+// TEMPLATE ENGINE
 
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+
+app.use(express.static('public'));
+
+// ROUTES
 app.get('/', (req, res) => {
+  res.render('index');
+});
 
-    const blog = 
-    {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description" 
-    }
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-    res.send(blog)
-})
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
 
 port = 3000;
 
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı`)
-})
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
