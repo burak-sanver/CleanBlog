b930e4c003815c43d46e74dcbeb6a68439419fd1
Your Name <you@example.com>
2022-06-28 01:04:05 +0300

    uploaded

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-const Post = require('./models/Post');
 
+
+const postController = require('./controllers/postControllers');
+const pageController = require('./controllers/pageController');
 const app = express();
 
 mongoose.connect('mongodb://localhost/cleanblog-test-db');
-
 
 // TEMPLATE ENGINE
 
@@ -17,36 +20,20 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+  methods:['POST', 'GET']
+}))
 
 // ROUTES
-app.get('/', async (req, res) => {
-  const posts = await Post.find({})
-  res.render('index', {
-    posts
-  });
-});
-
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post', { post })
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/post', (req, res) => {
-  res.render('post');
-});
-
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-app.post('/post', async (req, res) => {
-  await Post.create(req.body)
-  res.redirect('/');
-});
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/post', postController.createPost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost)
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
 port = 3000;
 
